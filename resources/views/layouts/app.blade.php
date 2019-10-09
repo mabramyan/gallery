4586aa1f8cea57bc36cfdf389b5ef16707b76586
mikheil <m.abramyan@fortunejack.com>
2019-10-09 15:38:14 +0400

    add styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,34 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body>
-    <div id="app">
+<body><div id="app">
+        <header id="main-header">
+                <div class="container flex-container">
+                    <div class="left-main-header">
+                        <ul>
+                            <li><a class="home-link" href="{{ url('/') }}">Home</a></li>
+                        </ul>
+                    </div>
+                    <div class="right-main-header">
+                        <form action="search.html" method="GET" class="search-form">
+                            <input class="search-input" type="text" placeholder="Search" name="search">
+                        </form>
+                        <ul>
+                            <li><a href="registration.html">Register</a></li>
+                            <li><a href="login.html">Login</a></li>
+                            <li class="dropdown-li" onClick="toggleUserDropdown();"><span class="user-image"><img  src="images/profile_default2.png"></span><span>Username</span><i id="dropdown-icon" class="fa fa-caret-down"></i>
+                                <ul id="user-dropdown-ul">
+                                    <li><a href="profile.html">Profile</a></li>
+                                    <!-- <li><a href="">Gallery</a></li>
+                                    <li><a href="">Settings</a></li> -->
+                                    <li><a href="logout">Sign Out</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+    
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
